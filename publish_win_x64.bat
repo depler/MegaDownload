00451e2aa23c03a8bf5ad340a03c61a8fd70b118
depler <depler.mv@gmail.com>
2023-02-22 17:39:56 +0300

    code

--- publish_win_x64.bat
+++ publish_win_x64.bat
@@ -1,1 +1,13 @@
-dotnet.exe publish --configuration "Release" --runtime "win-x64" --output "bin_win_x64" --self-contained -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true -p:PublishTrimmed=true -p:DebugType=None
+@echo off
+
+for %%f in (*.csproj) do (
+	set project=%%f
+	goto build
+)
+
+:error
+echo Project not found in current folder
+exit /b 1
+
+:build
+dotnet.exe publish %project% --configuration "Release" --runtime "win-x64" --output "bin_win_x64" --self-contained -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true -p:PublishTrimmed=true -p:DebugType=None
