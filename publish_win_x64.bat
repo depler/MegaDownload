a1d9d383f7a669fd1e129e71669e09943b91ef05
depler <depler.mv@gmail.com>
2023-02-24 12:08:46 +0300

    code

--- publish_win_x64.bat
+++ publish_win_x64.bat
@@ -11,3 +11,5 @@ exit /b 1
 
 :build
 dotnet.exe publish %project% --configuration "Release" --runtime "win-x64" --output "bin_win_x64" --self-contained -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true -p:PublishTrimmed=true -p:DebugType=None
+
+pause
